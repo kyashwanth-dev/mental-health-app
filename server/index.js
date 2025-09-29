3c8f74e73c381bf37f1425922dcfefb9215494d2
KATABATINI YASHWANTH KUMAR <kyashwanth831@gmail.com>
2025-09-29 19:34:20 +0530

    hatch

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://chatclient-kh67.onrender.com"],
+    origin: ["http://localhost:5173", "https://chatclient-kh67.onrender.com","https://chatspac.netlify.app"],
     methods: ["GET", "POST"]
   }
 });
